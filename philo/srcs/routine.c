30dccef946786d53412426b81fe25ce638d611f8
Crealex <alexandre.tomasi80@gmail.com>
2024-12-30 17:10:42 +0100

    commencement fonction monitor et auxilliaires

--- philo/srcs/routine.c
+++ philo/srcs/routine.c
@@ -3,14 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   routine.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alexandre <alexandre@student.42.fr>        +#+  +:+       +#+        */
+/*   By: atomasi <atomasi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 20:00:57 by alexandre         #+#    #+#             */
-/*   Updated: 2024/12/29 19:56:29 by alexandre        ###   ########.fr       */
+/*   Updated: 2024/12/30 16:18:00 by atomasi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
+
+int	check_eat(t_philo **philos)
+{
+	int i;
+
+	i = 0;
+	while (i < philos[i]->id)
+	{
+		// checker si count_eat est egal ou superieur a many_eat
+	}
+}
+
+int	check_death(t_philo **philos)
+{
+	int i;
+
+	i = 0;
+	while (i < philos[i]->id)
+	{
+		// checker si le temps depuis le dernier repas et superieur ou egal a tdie
+	}
+}
+
+void	*monitor(t_data *data)
+{
+	int i;
+
+	i = 0;
+	while (1)
+	{
+		if (check_death(data->philos))
+			return ;
+		if (data->many_eat > -1)
+		{
+			if (check_eat(data->philos))
+				return ;
+		}
+	}
+}
 
 void	*routine(t_philo *philo)
 {
@@ -32,10 +71,12 @@ void	*routine(t_philo *philo)
 void	create_routine(t_data *data, pthread_t **tid)
 {
 	int i;
+	pthread_t tid_monitor;
 
 	i = 0;
 	while (i < data->nb_philo)
 	{
 		pthread_create(tid[i], NULL, routine, &data->philos[i]);
 	}
+	pthread_create(&tid_monitor, NULL, monitor, data);
 }
